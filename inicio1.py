910c11a52044d474bdd790dceb65cbb276f2f6d2
jjrodmar <josejuanrodriguez@ieselrincon.es>
2020-04-26 13:13:56 +0100

    Primera Versión para los alumnos

---
+++ inicio1.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hola Don Pepito!"
+
+@app.route("/saludo")
+def saludo():
+    return "Soy Pepe Gotera"
+
+if __name__ == "__main__":
+    app.run()
